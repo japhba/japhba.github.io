a732552277ba44bf8025f4dd73bf346ced591e65
Jan Bauer <japhba@users.noreply.github.com>
2022-11-13 21:53:34 +0200

    Newline.

--- content/post/dmft/clean_post.py
+++ content/post/dmft/clean_post.py
@@ -5,4 +5,4 @@ fn = sys.argv[1]
 file = (Path(__file__).parent / fn)
 print(file.read_text())
 res = re.search(r"(---[\w\W]*---)", file.read_text())
-file.write_text(res.group(0))
+file.write_text(res.group(0) + "\n")
